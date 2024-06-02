1264b2e6cc95d5995944f0aee6b4dab3d1790c28
Esther White <monacodelisa@gmail.com>
2024-06-02 14:42:14 +0300

    another try

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,8 @@
 import {
-	afterRender,
-	AfterRenderPhase,
 	Component,
-	Inject,
-	inject,
 	OnInit,
-	PLATFORM_ID,
 } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-import { ThemeService } from "./services/theme.service";
-import { DOCUMENT, isPlatformBrowser } from "@angular/common";
 
 @Component({
 	selector: "app-root",
@@ -28,45 +21,8 @@ import { DOCUMENT, isPlatformBrowser } from "@angular/common";
 	],
 })
 export class AppComponent implements OnInit {
-	doc = inject(DOCUMENT);
-	activeTheme = "dark";
-
-	constructor(@Inject(PLATFORM_ID) private platformId: any) {
-		afterRender(
-			() => {
-				let themeLink = this.doc.createElement("link");
-				themeLink.id = "app-theme";
-				themeLink.rel = "stylesheet";
-				themeLink.href = this.setTheme();
-				this.doc.head.appendChild(themeLink);
-			},
-			{ phase: AfterRenderPhase.Write }
-		);
-	}
 
 	ngOnInit(): void {
 
-	}
-
-	getTheme() {
-		return this.activeTheme;
-	}
-
-	setTheme() {
-		if (this.prefersLightMode()) {
-			return "/src/light.scss";
-		} else {
-			return "/src/dark.scss";
-		}
-	}
-
-	prefersLightMode(): boolean {
-		if (isPlatformBrowser(this.platformId)) {
-			return (
-				window.matchMedia &&
-				window.matchMedia("(prefers-color-scheme: light)").matches
-			);
-		}
-		return false;
 	}
 }
